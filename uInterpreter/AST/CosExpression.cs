3ab1fc7a5ee2cfac1fbbfe3e852bf4e4b2c9dd10
木宛城主 <1194679215@qq.com>
2017-06-14 23:48:26 +0800

    增加一元表达式，Sin,Cos表达式

---
+++ uInterpreter/AST/CosExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uInterpreter.AST
+{
+    public class CosExpression:Expression
+    {
+        private Expression _expression;
+
+        public CosExpression(Expression expression)
+        {
+            _expression = expression;
+        }
+        public override double Evaluate()
+        {
+            var value = _expression.Evaluate();
+            return Math.Cos(value);
+        }
+    }
+}
---
+++ uInterpreter/AST/UnaryExpression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uInterpreter.Enum;
+
+namespace uInterpreter.AST
+{
+    public class UnaryExpression:Expression
+    {
+        private Expression _expression;
+        private Operator _operator;
+
+        public UnaryExpression(Expression expression)
+        {
+            _expression = expression;
+        }
+        public override double Evaluate()
+        {
+            switch (_operator)
+            {
+                case Operator.Plus:
+                    return _expression.Evaluate();
+                case Operator.Minus:
+                    return -_expression.Evaluate();
+            }
+            return Double.NaN;
+        }
+    }
+}
