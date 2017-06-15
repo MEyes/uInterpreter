368d1f4b8a2d87957e4f1b3da733ac7e401dfaae
木宛城主 <1194679215@qq.com>
2017-06-15 14:23:56 +0800

    Parser初构

---
+++ uInterpreter/Parser/Parser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uInterpreter.AST;
+
+namespace uInterpreter.Parser
+{
+    public class Parser:Lexer
+    {
+        private Token _currentToken;
+
+
+        public Parser(string expressionStr) : base(expressionStr)
+        {
+        }
+
+        public Expression CallExpr()
+        {
+            _currentToken = GeToken();
+            return Expr();
+        }
+
+        private Expression Expr()
+        {
+            return null;
+        }
+
+        private Expression Term()
+        {
+            return null;
+        }
+
+        private Expression Factor()
+        {
+            return null;
+        }
+
+
+
+    }
+}
