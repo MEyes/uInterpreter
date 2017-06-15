918dd59b4b8c77462db538401337eb74bda01b30
木宛城主 <1194679215@qq.com>
2017-06-16 00:04:44 +0800

    增加Random表达式

--- uInterpreter/AST/RandomExpression.cs
+++ uInterpreter/AST/RandomExpression.cs
@@ -8,9 +8,17 @@ namespace uInterpreter.AST
 {
     public class RandomExpression:Expression
     {
+        private int _minValue;
+        private int _maxValue;
+        public RandomExpression(int minValue,int maxValue)
+        {
+            this._minValue = minValue;
+            _maxValue = maxValue;
+        }
         public override double Evaluate()
         {
-            
+            Random random=new Random();
+            return Convert.ToDouble(random.Next(_minValue, _maxValue));
         }
     }
 }
