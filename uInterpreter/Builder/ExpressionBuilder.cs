ee9473084c592ba0a377c67616a6a401b30f4286
木宛城主 <1194679215@qq.com>
2017-06-15 15:23:44 +0800

    Builder构造器

---
+++ uInterpreter/Builder/ExpressionBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uInterpreter.AST;
+
+namespace uInterpreter.Builder
+{
+    public class ExpressionBuilder:AbstractBuilder
+    {
+        private string _expressionStr;
+
+        public ExpressionBuilder(string expressionStr)
+        {
+            _expressionStr = expressionStr;
+        }
+
+        public Expression GetExpression()
+        {
+            try
+            {
+                Parser.Parser parser=new Parser.Parser(_expressionStr);
+                return parser.CallExpr();
+            }
+            catch (Exception)
+            {
+                
+                throw new Exception("error");
+            }
+        }
+    }
+}
